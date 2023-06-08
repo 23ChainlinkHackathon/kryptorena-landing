ced57172513d1819e052f692e42ad3bcb97a1992
Nanasark <danielnyamekyesarkodie@gmail.com>
2023-06-08 11:10:48 +0000

    max changes

--- src/landingcomps/Fray.jsx
+++ src/landingcomps/Fray.jsx
@@ -5,7 +5,9 @@ const Fray = () => {
     <div id="fray" className="lg:w-auto w-screen overflow-hidden xsm:bg-[url('./img/fray.png')] lg:bg-[url('./img/fraylg.png')] md:bg-[url('./img/fraymd.png')] bg-cover  h-[438px] flex flex-col gap-5 justify-center items-center">
         <h1 className='header relative lg:text-[48px] xsm:text-[32px] self-stretch text-[#F4DDB6] drop-shadow-[0_4px_16px_rgba(228,172,78,0.6)]'>Ascend to greatness</h1>
         {/* <p className='text-white lg:text-[18px] '> Fantasy is create</p> */}
+        <a href="https://kryptorena.netlify.app/">
         <button className='bg-[#514633] header ring-1 ring-[#BBA278] rounded-[8px] text-[#DFC28E] xsm:left-[80px] xsm:w-[140px] xsm:h-[40px]' > Play Game</button>
+        </a>
     </div>
   )
 }
